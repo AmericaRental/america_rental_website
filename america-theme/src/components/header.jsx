c17e77360473b6747f776b4882effc99c010f7a2
América Rental - T.I <106966936+AmericaRental@users.noreply.github.com>
2022-06-24 16:13:25 -0300

    :construction: :lipstick: header updates

--- america-theme/src/components/header.jsx
+++ america-theme/src/components/header.jsx
@@ -9,12 +9,13 @@ import linkedin from "../images/linkedin.svg";
 import whatsapp from "../images/whatsapp.svg";
 
 const StyledHeader = styled.header`
-  width: 100%;
+  width: 100vw;
   height: fit-content;
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 10px;
+  justify-content: space-between;
+  padding: 0.5rem 0px;
 `;
 
 const SocialHeader = styled.section`
@@ -25,12 +26,13 @@ const SocialHeader = styled.section`
   flex-direction: row;
   align-content: center;
   align-items: center;
-  justify-content: center;
+  justify-content: space-evenly;
 `;
 
 const LogoAmerica = styled.img`
   width: 18%;
   height: auto;
+  margin: 0px 0.8rem;
 `;
 LogoAmerica.defaultProps = {
   src: vetor_logo_america_colorido,
@@ -47,13 +49,22 @@ const Div = styled.div`
   height: fit-content;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: ${props => props.jContent || 'Center'};
   align-items: center;
 `;
 
 const Img = styled.img`
   width: 18px;
   height: 18px;
+`;
+
+const Button = styled.button`
+    width: fit-content;
+    height: fit-content;
+    background-color: transparent;
+    color: #515151;
+    border: none;
+    
 `;
 
 export default class Header extends React.Component {
@@ -63,31 +74,31 @@ export default class Header extends React.Component {
         {/* mídias sociais acima do header */}
         <SocialHeader>
           <Div>
-            <Img src={email} alt="email " />
+            <Img src={email} alt="email" />
             <Paragraph cor="#FAFAFA" tamanho="1.05rem" padding="5px">
               vendas@americarental.com.br
             </Paragraph>
           </Div>
           <Div>
-            <Img src={email} alt="email " />
+            <Img src={whatsapp} alt="whatsapp" />
             <Paragraph cor="#FAFAFA" tamanho="1.05rem" padding="5px">
               (11) 2499-3353
             </Paragraph>
           </Div>
           <Div>
-            <Img src={email} alt="email " />
+            <Img src={facebook} alt="facebook" />
             <Paragraph cor="#FAFAFA" tamanho="1.05rem" padding="5px">
               /AmericaRental
             </Paragraph>
           </Div>
           <Div>
-            <Img src={email} alt="email " />
+            <Img src={instagram} alt="instagram" />
             <Paragraph cor="#FAFAFA" tamanho="1.05rem" padding="5px">
               @america_rental
             </Paragraph>
           </Div>
           <Div>
-            <Img src={email} alt="email " />
+            <Img src={linkedin} alt="Linkedin" />
             <Paragraph cor="#FAFAFA" tamanho="1.05rem" padding="5px">
               /AmericaRental
             </Paragraph>
@@ -97,6 +108,14 @@ export default class Header extends React.Component {
         {/* Header  */}
         <StyledHeader>
           <LogoAmerica />
+          {/* menu */}
+          <Div jContent="space-around">
+            <Button>Home</Button>
+            <Button>Quem Somos</Button>
+            <Button>Orçamento</Button>
+            <Button>Produtos</Button>
+            <Button>Contato</Button>
+          </Div>
         </StyledHeader>
       </>
     );
