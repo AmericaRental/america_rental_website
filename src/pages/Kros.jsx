db461f3813da15443752198761368261231ea2fb
Murilo Cardoso <65872392+5gMurilo@users.noreply.github.com>
2022-08-08 12:51:20 -0300

    kros pagw

--- src/pages/Kros.jsx
+++ src/pages/Kros.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import WhatsappButton from '../components/global/whatsapp_button'
+import React from "react";
+import Product from "../components/global/product";
+import WhatsappButton from "../components/global/whatsapp_button";
+import banner from "../images/pia/banner.svg";
 
+import MIC from '../images/quimicos/kros/MIC.png';
+import img1 from '../images/quimicos/kros/Mictorio1-1.jpg';
+import img2 from '../images/quimicos/kros/Mictorio2.jpg';
 function Kros() {
   return (
-    <div>kros
-        <WhatsappButton />
+    <>
+      <WhatsappButton />
 
-    </div>
-  )
+      <Product
+        banner={[banner]}
+        prodImg={MIC}
+        type="Modelos Portáteis"
+        name="Mictório Kros"
+        description={[
+          "Os mictórios são ",
+          <strong>ideais para eventos de grande porte</strong>,
+          " com um grande fluxo de pessoas.",
+          <br />,
+          <br />,
+          "Por ter um uso mais rápido e capacidade de até 4 usuários por vez, ",
+          <strong>auxilia na diminuição da fila do banheiro.</strong>,
+        ]}
+        imgs={[img1, img2]}
+        specs={[
+          "Cada torre possui 4 mictórios",
+          "Caixa de dejetos com capacidade de até 450L",
+          'Pode ser facilmente ligado a rede de esgoto',
+          'Piso antiderrapante',
+          'Economia de espaço',
+          'Diminuição da fila do banheiro'
+        ]}
+        prodDimension={"A 2,00m X L 1,15m X P 1,15m"}
+        prodWeight={"85kg"}
+      />
+    </>
+  );
 }
 
-export default Kros
+export default Kros;
--- src/pages/quimicos/pne.jsx
+++ src/pages/quimicos/pne.jsx
@@ -7,28 +7,37 @@ import img3 from "../../images/quimicos/pne/3.jpg";
 import img4 from "../../images/quimicos/pne/4.jpg";
 
 import banner from "../../images/pia/banner.svg";
+import WhatsappButton from "../../components/global/whatsapp_button";
 
 function PNE() {
   return (
-    <Product
-      banner={[banner]}
-      prodImg={img1}
-      type="Cabines de banheiro químico"
-      name="Modelo PNE"
-      description={[
-        "As cabines de ",
-        <strong>modelo PNE (Portadores de Necessidades Especiais)</strong>,
-        " são ideais para eventos e obras inclusivas",
-        <br/>,
-        <br/>,
-        "A ", <strong>quantidade de limpezas</strong>," é determinada pela ", <strong>quantidade médida de usuários</strong>,", sendo o programa mínimo composto por 1 limpeza por semana, não podendo ser menos pois esta é a validade do produto químico usado.",
-        <br/>,
-        <br/>,
-        "Na ",<strong>manutenção</strong>," é realizada a reposição dos insumos, sucção do tanque e a limpeza interna da cabine."
-      ]}
-      imgs={[img2,img3,img4]}
-      specs={
-        [
+    <>
+      <WhatsappButton />
+
+      <Product
+        banner={[banner]}
+        prodImg={img1}
+        type="Cabines de banheiro químico"
+        name="Modelo PNE"
+        description={[
+          "As cabines de ",
+          <strong>modelo PNE (Portadores de Necessidades Especiais)</strong>,
+          " são ideais para eventos e obras inclusivas",
+          <br />,
+          <br />,
+          "A ",
+          <strong>quantidade de limpezas</strong>,
+          " é determinada pela ",
+          <strong>quantidade médida de usuários</strong>,
+          ", sendo o programa mínimo composto por 1 limpeza por semana, não podendo ser menos pois esta é a validade do produto químico usado.",
+          <br />,
+          <br />,
+          "Na ",
+          <strong>manutenção</strong>,
+          " é realizada a reposição dos insumos, sucção do tanque e a limpeza interna da cabine.",
+        ]}
+        imgs={[img2, img3, img4]}
+        specs={[
           "Caixa de dejetos com assento",
           "Piso rebaixado com mini rampa para cadeira de rodas",
           "Suporte de papel higiênico",
@@ -38,12 +47,12 @@ function PNE() {
           "Trinco indicador Ocupado e Livre",
           "Ventilação interna por telas",
           "Piso antiderrapante",
-          "Identificação Masculino e Feminino"
-        ]
-      }
-      prodDimension={"A 2,35m X L 1,10m X P 1,80m"}
-      prodWeight={"102kg"}
-    />
+          "Identificação Masculino e Feminino",
+        ]}
+        prodDimension={"A 2,35m X L 1,10m X P 1,80m"}
+        prodWeight={"102kg"}
+      />
+    </>
   );
 }
 
